be47c15ae4f2dc6d5cdb4cd2e2f272163ab1422f
naylacamelia <naylacame@gmail.com>
2026-08-06 17:55:24 +0800

    configure cloudflare pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui','@nuxt/image'],
-  css:['@/assets/css/main.css']
+  modules: ['@nuxt/ui','@nuxt/image','@nuxthub/core'],
+  css:['@/assets/css/main.css'],
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
+  
+  // hub: {
+  //   database: true, // Mengaktifkan Cloudflare D1 (Database SQL)
+  //   blob: true,     // Mengaktifkan Cloudflare R2 (Upload Gambar/File)
+  //   kv: true        // Mengaktifkan Cloudflare KV (Cache)
+  // }
 })
