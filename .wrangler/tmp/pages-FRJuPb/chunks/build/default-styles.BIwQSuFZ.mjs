009b93da34180a04dcc8c63fabe7b513217d630a
naylacamelia <naylacame@gmail.com>
2026-08-07 08:18:14 +0800

    backend integrated

--- .wrangler/tmp/pages-FRJuPb/chunks/build/default-styles.BIwQSuFZ.mjs
+++
@@ -1,2 +0,0 @@
-const a=[".fade-enter-active[data-v-84660acb],.fade-leave-active[data-v-84660acb]{transition:all .15s}.fade-enter-from[data-v-84660acb],.fade-leave-to[data-v-84660acb]{opacity:0;transform:translate(-6px)}"];export{a as default};
-//# sourceMappingURL=default-styles.BIwQSuFZ.mjs.map
--- .wrangler/tmp/pages-FRJuPb/chunks/virtual/styles.mjs
+++
@@ -1,2 +0,0 @@
-const interopDefault=t=>t.default||t||[],t={"layouts/default.vue":()=>import("../build/default-styles.BIwQSuFZ.mjs").then(interopDefault),"components/layout/Sidebar.vue":()=>import("../build/default-styles.BIwQSuFZ.mjs").then(interopDefault)};export{t as default};
-//# sourceMappingURL=styles.mjs.map
